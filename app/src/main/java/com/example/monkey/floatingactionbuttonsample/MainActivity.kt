5f86b3ee1b9616bb4e46c688d05a9d975a86b868
monkey <gikeda@appirits.com>
2017-11-29 18:02:15 +0900

    [add] 長押しでアイコンを変更

---
+++ app/src/main/java/com/example/monkey/floatingactionbuttonsample/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.monkey.floatingactionbuttonsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
+import android.support.v7.appcompat.R.attr.color
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.security.AccessController.getContext
+import android.widget.Toast
+import android.R.id.button1
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<FloatingActionButton>(R.id.fabActivityMain)
+
+        button.setOnLongClickListener(View.OnLongClickListener {
+            button.setImageResource(R.drawable.ic_launcher_background)
+            true    // 戻り値をtrueにするとOnClickイベントは発生しない
+        })
+    }
+}
